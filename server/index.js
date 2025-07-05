907da651c23db434a4f608491d3c09729cdd7d4d
Rahul Shrivastav <rahul.shrivastav.226169@gmail.com>
2025-07-05 12:03:14 +0530

    Temp

--- server/index.js
+++ server/index.js
@@ -30,12 +30,14 @@ app.use("/api/auth",userRoutes);
 app.use("/api/product",productRoutes)
 app.use("/api/cart",cartRoutes)
 app.use('/api/order',orderRoutes)
-   
+
+if(process.env.NODE_ENV=="prod"){
     app.use(express.static(path.join(__dirname,"../client/dist")));
 
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../client","dist","index.html"))
     })
+}
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,7 +1,12 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req,res,next)=>{    
-    const token = req.headers['authorization'].split(" ")[1]; 
+    const authHeader = req.headers['authorization'];
+    if(!authHeader){
+        return res.status(403).json({message:"Authorization header is required"});
+    }
+    
+    const token = authHeader.split(" ")[1]; 
     if(!token){
         return res.status(403).json({message:"Token is required"});
     }
