d4572085894a8f267bc7ea642a884817c65ca5ba
Rahul Shrivastav <rahul.shrivastav.226169@gmail.com>
2025-07-05 11:58:03 +0530

    Deploy ready

--- server/index.js
+++ server/index.js
@@ -30,16 +30,12 @@ app.use("/api/auth",userRoutes);
 app.use("/api/product",productRoutes)
 app.use("/api/cart",cartRoutes)
 app.use('/api/order',orderRoutes)
-
-if(process.env.NODE_ENV=="prod"){
-    console.log(process.env.NODE_ENV);
-    
+   
     app.use(express.static(path.join(__dirname,"../client/dist")));
 
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../client","dist","index.html"))
     })
-}
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
